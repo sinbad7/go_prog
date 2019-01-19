93beb3db33882edeed92eb554f1f8097e1ae97a1
grigvlwork <grigvlwork@gmail.com>
2019-01-19 21:17:42 +0700

    structs done

--- structs/structs.go
+++ structs/structs.go
@@ -1,61 +1,60 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 type Person struct {
 	FirstName, LastName string
-	Dob 		    time.Time
+	Dob                 time.Time
 	Email, Location     string
 }
 
-
 func (p Person) PrintName() {
-    fmt.Printf("\n%s %s\n", p.FirstName, p.LastName)
-}    
-
-func (p Person) PrintDetails(){
-    fmt.Printf("[Date of Birth: %s, Email: %s, Location: %s]\n", p.Dob.String(), p.Email, p.Location )
+	fmt.Printf("\n%s %s\n", p.FirstName, p.LastName)
 }
 
-func (p *Person) ChangeLocation(newLocation string){
-    p.Location= newLocation
+func (p Person) PrintDetails() {
+	fmt.Printf("[Date of Birth: %s, Email: %s, Location: %s]\n", p.Dob.String(), p.Email, p.Location)
+}
+
+func (p *Person) ChangeLocation(newLocation string) {
+	p.Location = newLocation
 }
 
 type Admin struct {
-    Person
-    Roles []string
+	Person
+	Roles []string
 }
 
 type Member struct {
-    Person
-    Skills []string
+	Person
+	Skills []string
 }
 
 func main() {
-    alex :=Admin {
-	   Person{
-	          "Alex",
-	          "John",
-	          time.Date(1970, time.January, 0,0,0,0,0, time.UTC),
-	          "alex@email.com",
-	          "New York",
-	          },
-	          []string("Manage Team", "Manage Tasks"),
-	   }
-    shiju := Member {
-        Person{
-	"Shiju",
-	"Varghese",
-	time.Date(1979, time.February, 0,0,0,0,0, time.UTC),
-	"shiju@email.com",
-	"Kochi"},
-	[]string("Go", "Docker", "Kubernetes"),
-    }
-    alex.PrintName()
-    alex.PrintDetails()
-    shiju.PrintName()
-    shiju.PrintDetails()
+	alex := Admin{
+		Person{
+			"Alex",
+			"John",
+			time.Date(1970, time.January, 0, 0, 0, 0, 0, time.UTC),
+			"alex@email.com",
+			"New York",
+		},
+		[]string{"Manage Team", "Manage Tasks"},
+	}
+	shiju := Member{
+		Person{
+			"Shiju",
+			"Varghese",
+			time.Date(1979, time.February, 0, 0, 0, 0, 0, time.UTC),
+			"shiju@email.com",
+			"Kochi"},
+		[]string{"Go", "Docker", "Kubernetes"},
+	}
+	alex.PrintName()
+	alex.PrintDetails()
+	shiju.PrintName()
+	shiju.PrintDetails()
 }
