b6833c47c9c2d02bbf11abf545e03adcab94cfc6
grigvlwork <grigvlwork@gmail.com>
2019-01-21 14:24:38 +0700

    handlerfunc 1

---
+++ handlerfunc/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Go Web Development")
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mh := http.HandlerFunc(messageHandler)
+	mux.Handle("/welcome", mh)
+	log.Println("Listening")
+	http.ListenAndServe(":8080", mux)
+}
